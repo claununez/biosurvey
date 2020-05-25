825999f8bf3c0e214a5eed48bb52e92607123736
claununez <claununez199o@gmail.com>
2020-05-24 23:39:36 -0500

    V 0.0.3 function to match raster format

--- R/short_helpers.R
+++ R/short_helpers.R
@@ -10,3 +10,33 @@
 is_pair <- function(x) {
   x / 2 == as.integer(x / 2)
 }
+
+
+
+
+
+#' Helper function to find raster extension
+#'
+#' @param format (character) any of the format types allowed for raster objects.
+#' See \code{\link[raster]{writeFormats}} (e.g., "GTiff").
+#'
+#' @return Raster extension according to format type.
+#'
+#' @export
+#'
+#' @examples
+#' rformat <- match_rformat("GTiff")
+
+match_rformat <- function(format) {
+  if (missing(format)) {stop("Argument 'format needs to be defined.")}
+  if (format == "raster") {format1 <- ".grd"}
+  if (format == "GTiff") {format1 <- ".tif"}
+  if (format == "EHdr") {format1 <- ".bil"}
+  if (format == "ascii") {format1 <- ".asc"}
+  if (format == "SAGA") {format1 <- ".sdat"}
+  if (format == "IDRISI") {format1 <- ".rst"}
+  if (format == "CDF") {format1 <- ".nc"}
+  if (format == "ENVI") {format1 <- ".envi"}
+  if (format == "HFA") {format1 <- ".img"}
+  return(format1)
+}
