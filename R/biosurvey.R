cc68527b96cae111dd7ce2dce3d3ddd76b4b54af
claununez <claununez199o@gmail.com>
2020-05-27 00:36:31 -0500

    V 0.0.3 package documentation

---
+++ R/biosurvey.R
@@ -0,0 +1,18 @@
+#' biosurvey: Tools for Biological Survey Planning
+#'
+#' nichevol is a collection of tools that allow users to select sampling sites
+#' increasing efficiency of biodiversity survey systems by considering the
+#' relationship of environmental and geographic conditions in a region.
+#'
+#' @section Main functions in biosurvey:
+#' \code{\link{master_matrix}}, \code{\link{base_pam}}
+#'
+#' Other functions (important helpers)
+#'
+#' \code{\link{files_2data}}, \code{\link{grid_from_region}},
+#' \code{\link{pam_from_table}}, \code{\link{rlist_2data}},
+#' \code{\link{spdf_2data}}, \code{\link{stack_2data}}
+#'
+#' @docType package
+#' @name biosurvey
+NULL
