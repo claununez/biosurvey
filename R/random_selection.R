47b85fc99c21d2fdc367e632312231006926909f
claununez <claununez199o@gmail.com>
2020-07-06 23:13:39 -0500

    V 0.0.15 median_distance_filter added to random selection

--- R/random_selection.R
+++ R/random_selection.R
@@ -11,6 +11,9 @@
 #' \code{master_matrix} to be used as sites to be sampled in survey.
 #' @param n_samplings (numeric) number of processes of selection, which will
 #' turn into multiple options for a process of survey planning. Default = 1.
+#' @param median_distance_filter (character) optional argument to define a median
+#' distance-based filter based on which sets of sampling sites will be selected.
+#' The default, NULL, does not apply such a filter. Options are: "max" and "min".
 #' @param set_seed (numeric) integer value to specify a initial seed. Default = 1.
 #'
 #' @return
@@ -34,9 +37,11 @@
 #' \code{\link{EG_selection}}, \code{\link{plot_sites_EG}}
 #'
 #' @usage
-#' random_selection(master, n_sites, n_samplings = 1, set_seed = 1)
+#' random_selection(master, n_sites, n_samplings = 1,
+#'                  median_distance_filter = NULL, set_seed = 1)
 #'
 #' @export
+#' @importFrom stats median
 #'
 #' @examples
 #' # Data
@@ -44,7 +49,8 @@
 #'
 #' r_selection <- random_selection(m_matrix, n_sites = 20, n_samplings = 5)
 
-random_selection <- function(master, n_sites, n_samplings = 1, set_seed = 1) {
+random_selection <- function(master, n_sites, n_samplings = 1,
+                             median_distance_filter = NULL, set_seed = 1) {
 
   # Initial tests
   if (missing(master)) {
@@ -53,6 +59,13 @@ random_selection <- function(master, n_sites, n_samplings = 1, set_seed = 1) {
   if (missing(n_sites)) {
     stop("Argument 'n_sites' must be defined.")
   }
+  if (!is.null(median_distance_filter)) {
+    if (!median_distance_filter %in% c("max", "min")) {
+      stop("Argument 'median_distance_filter' is not valid, see function's help.")
+    }
+  }
+
+  # Selection of sites
   data <- master$master_matrix
 
   selected_sites <- lapply(1:n_samplings, function(x) {
@@ -62,6 +75,22 @@ random_selection <- function(master, n_sites, n_samplings = 1, set_seed = 1) {
   })
   names(selected_sites) <- paste0("selection_", 1:n_samplings)
 
+  # Post filtering of sites according to distance argument
+  if (length(selected_sites) > 1 & !is.null(median_distance_filter)) {
+    dists <- sapply(selected_sites, function(x) {
+      dis <- raster::pointDistance(x[, c("Longitude", "Latitude")], lonlat = TRUE)
+      diag(dis) <- NA
+      median(c(dis), na.rm = T)
+    })
+
+    if (median_distance_filter == "max") {
+      selected_sites <- selected_sites[dists == max(dists)]
+    } else {
+      selected_sites <- selected_sites[dists == min(dists)]
+    }
+  }
+
+  # Returning results
   master$selected_sites_random <- selected_sites
 
   return(structure(master, class = "master_selection"))
