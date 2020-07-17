e861e14f35d6d66d6ebae978d315c3aa98cd8e61
claununez <claununez199o@gmail.com>
2020-07-16 22:54:06 -0500

    V 0.0.19 using mask to reduce region

--- R/master_matrix.R
+++ R/master_matrix.R
@@ -4,8 +4,12 @@
 #' further analyses. This object will contain geographic and environmental
 #' information that will be used to characterize the region of interest.
 #'
-#' @param region SpatialPolygonsDataFrame of the region of interest.
+#' @param region SpatialPolygons* of the region of interest; for instance,
+#' a country, another type of administrative are, or a protected area.
 #' @param variables RasterStack or RasterBrick of environmental variables.
+#' @param mask (optional) SpatialPolygons* object to mask \code{variables} and
+#' reduce \code{region} to an area that is more relevant for analysis (e.g., only
+#' areas with natural vegetation cover). Default = NULL.
 #' @param do_pca (logical) whether or not to perform a Principal Component Analysis.
 #' Default = FALSE.
 #' @param center (logical) whether or not to center variables. Argument to be passed
@@ -21,18 +25,29 @@
 #' - master_matrix: a date.frame with information about geographic location of
 #' raster cells, initial environmental data, and if \code{do_pca} is TRUE,
 #' the first two principal components derived from original data.
-#' - polygon: a SpatialPolygonsDataFrame representing the region of interest.
+#' - region: a SpatialPolygons* representing the region of interest.
 #' - raster_base: a raster layer for the region of interest with a single value,
 #' to be used for plotting purposes.
+#' - mask: SpatialPolygons* object used. Present only if \code{mask} was defined.
 #' - PCA_results: if \code{do_pca} is TRUE, other results from principal
 #' component analysis. If FALSE, PCA_results element of the object is NULL.
 #'
+#' @details
+#' This function helps in preparing all data as needed for further analyses
+#' aiming to define a survey sampling system considering geographic and
+#' environmental spaces in the \code{region} of interest.
+#'
+#' If \code{mask} is defined all analyses will be restricted to such an area. If
+#' \code{mask} is not fully contained by \code{region}, the mask used for reducing
+#' \code{variables}, and returned as part of the S3 object (master_matrix) is the
+#' intersection between them.
+#'
 #' @usage
-#' master_matrix(region, variables, do_pca = FALSE, center = TRUE, scale = FALSE,
-#'               variables_in_matrix = NULL)
+#' master_matrix(region, variables, mask = NULL, do_pca = FALSE, center = TRUE,
+#'               scale = FALSE, variables_in_matrix = NULL)
 #'
 #' @export
-#' @importFrom raster mask crop rasterToPoints
+#' @importFrom raster mask crop rasterToPoints intersect
 #' @importFrom stats prcomp
 #'
 #' @examples
@@ -46,27 +61,36 @@
 #'                           center = TRUE, scale = TRUE)
 
 
-master_matrix <- function(region, variables, do_pca = FALSE, center = TRUE,
-                          scale = FALSE, variables_in_matrix = NULL) {
+master_matrix <- function(region, variables, mask = NULL, do_pca = FALSE,
+                          center = TRUE, scale = FALSE, variables_in_matrix = NULL) {
   # Initial tests
   if (missing(region)) {
     stop("Argument 'region' must be defined")
   }
-
-  if (class(region)[1] != "SpatialPolygonsDataFrame") {
-    stop("'region' must be of class SpatialPolygonsDataFrame")
+  if (!class(region)[1] %in% c("SpatialPolygons", "SpatialPolygonsDataFrame")) {
+    stop("'region' must be of class SpatialPolygons*")
   }
-
   if (missing(variables)) {
     stop("Argument 'variables' must be defined")
   }
-
   if (!class(variables)[1] %in% c("RasterStack", "RasterBrick")) {
     stop("'variables' must be of class RasterStack or RasterBrick")
   }
+  if (!is.null(mask)) {
+    if (!class(mask)[1] %in% c("SpatialPolygons", "SpatialPolygonsDataFrame")) {
+      stop("'mask' must be of class SpatialPolygons*")
+    }
+
+    # Intersection of region and mask to avoid complications
+    mask <- raster::intersect(region, mask)
+  }
 
   # Mask variables to polygons (region of interest)
-  variables <- raster::mask(raster::crop(variables, region), region)
+  if (!is.null(mask)) {
+    variables <- raster::mask(raster::crop(variables, mask), mask)
+  } else {
+    variables <- raster::mask(raster::crop(variables, region), region)
+  }
 
   # Base raster
   b_raster <- variables[[1]]
@@ -90,7 +114,7 @@ master_matrix <- function(region, variables, do_pca = FALSE, center = TRUE,
     master_m <- data.frame(variables, pca$x[, 1:2])
 
     # Return results
-    return(structure(list(master_matrix = master_m, polygon = region,
+    return(structure(list(master_matrix = master_m, polygon = region, mask = mask,
                           raster_base = b_raster, PCA_results = pca),
                      class = "master_matrix"))
 
@@ -99,7 +123,7 @@ master_matrix <- function(region, variables, do_pca = FALSE, center = TRUE,
     master_m <- data.frame(variables)
 
     # Return results
-    return(structure(list(master_matrix = master_m, polygon = region,
+    return(structure(list(master_matrix = master_m, region = region, mask = mask,
                           raster_base = b_raster, PCA_results = NULL),
                      class = "master_matrix"))
   }
