ecbe680fee1340b8fb184df10fa86fcb0f20e3e7
claununez <claununez199o@gmail.com>
2020-07-28 01:21:49 -0500

    V 0.0.21 description and index computation improved

--- R/PAM_indices.R
+++ R/PAM_indices.R
@@ -8,7 +8,7 @@
 #' rows and species in the columns. See details.
 #' @param indices (character) code for indices to be calculated. Basic indices
 #' are calculated all the time, other indices need to be specified. Options are:
-#' "all", "basic, "AB", "BW", "BL", "SCSC", "SCSR", "DF", "CC", "WRN", "SRC", 
+#' "all", "basic, "AB", "BW", "BL", "SCSC", "SCSR", "DF", "CC", "WRN", "SRC",
 #' "CMSC", and "CMSR". See details. Default = "all"
 #' @param exclude_column (optional) name or numeric index of columns to be
 #' excluded. Default = NULL.
@@ -23,27 +23,29 @@
 #' Description of the codes of all indices to be calculated is presented in the
 #' table below. If \code{indices} = "basic", only basic indices are calculated.
 #' However, basic indices are calculated in all cases not matter the code(s)
-#' defined in \code{indices}. For further details on the way calculations are
-#' performed and the meaning of the indices see Soberon and Cavner (2015)
+#' defined in \code{indices}. Some indices require previous calculations of other
+#' indices, in such cases, all indices required are added to the final list.
+#' For further details on the way calculations are performed and the meaning of
+#' the indices see Soberon and Cavner (2015)
 #' \doi{https://doi.org/10.17161/bi.v10i0.4801}.
 #'
-#' |Code  |Index                                    |Calculation            |
-#' |:-----|----------------------------------------:|----------------------:|
-#' |      |Richness                                 |Basic                  |
-#' |      |Range                                    |Basic                  |
-#' |      |Richness standarized                     |Basic                  |
-#' |      |Range standarized                        |Basic                  |
-#' |AB    |Additive Beta                            |Needs to be defined    |
-#' |BW    |Beta Whittaker                           |Needs to be defined    |
-#' |BL    |Beta Legendre                            |Needs to be defined    |
-#' |SCSC  |Schluter covariance sites-composition    |Needs to be defined    |
-#' |SCSR  |Schluter covariance species-ranges       |Needs to be defined    |
-#' |DF    |Dispersal field                          |Needs to be defined    |
-#' |SCC   |Shared community composition             |Needs to be defined    |
-#' |WRN   |Wright-Reeves nestedness                 |Needs to be defined    |
-#' |SRC   |Stone-Roberts Cscore                     |Needs to be defined    |
-#' |CMSC  |Covariance matrix sites-composition      |Needs to be defined    |
-#' |CMSR  |Covariance matrix species-ranges         |Needs to be defined    |
+#' |Code  |Index                                    |Calculation                    |
+#' |:-----|----------------------------------------:|------------------------------:|
+#' |      |Richness                                 |Basic                          |
+#' |      |Range                                    |Basic                          |
+#' |      |Richness standarized                     |Basic                          |
+#' |      |Range standarized                        |Basic                          |
+#' |AB    |Additive Beta                            |Needs to be defined            |
+#' |BW    |Beta Whittaker                           |Needs to be defined            |
+#' |BL    |Beta Legendre                            |Needs to be defined and DF     |
+#' |SCSC  |Schluter covariance sites-composition    |Needs to be defined and CMSC   |
+#' |SCSR  |Schluter covariance species-ranges       |Needs to be defined and CMSR   |
+#' |DF    |Dispersal field                          |Needs to be defined            |
+#' |SCC   |Shared community composition             |Needs to be defined            |
+#' |WRN   |Wright-Reeves nestedness                 |Needs to be defined, BW, and DF|
+#' |SRC   |Stone-Roberts Cscore                     |Needs to be defined and DF     |
+#' |CMSC  |Covariance matrix sites-composition      |Needs to be defined            |
+#' |CMSR  |Covariance matrix species-ranges         |Needs to be defined            |
 #'
 #' @usage
 #' PAM_indices(PAM, indices = "all", exclude_column = NULL)
@@ -65,13 +67,13 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
   if (missing(PAM)) {
     stop("Argument 'PAM' must be defined.")
   }
-  
-  all_in <- c("all", "basic", "AB", "BW", "BL", "SCSC", "SCSR", "DF", "SCC", 
+
+  all_in <- c("all", "basic", "AB", "BW", "BL", "SCSC", "SCSR", "DF", "SCC",
               "WRN", "SRC", "CMSC", "CMSR")
   if (any(!indices %in% all_in)) {
     stop("One or more elements defined in 'indices' is not valid, check function's help.")
   }
-  
+
   # More tests and preparing data
   cpam <- class(PAM)[1]
   if (!cpam %in% c("base_PAM", "matrix", "data.frame")) {
@@ -84,13 +86,13 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
       exclude_column <- NULL
     }
   }
-  
+
   if (!is.null(exclude_column)) {
     ## test class of exclude column
     if (class(!exclude_column)[1] %in% c("numeric", "character")) {
       stop("Argument 'exclude_column' must be of class 'numeric' or 'character'.")
     }
-    
+
     ## process matrix to exclude
     if (is.numeric(exclude_column)) {
       PAM <- PAM[, -exclude_column]
@@ -98,38 +100,38 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
       PAM <- PAM[, !colnames(PAM) %in% exclude_column]
     }
   }
-  
+
   if (cpam == "data.frame") {
     PAM <- as.matrix(PAM)
   }
-  
+
   # PAM transpose
   tm1 <- t(PAM)
-  
+
   # PAM properties
   S <- ncol(PAM)
   N <- nrow(PAM)
-  
+
   # matrices A and Omega
   A <- PAM %*% tm1
   O <- tm1 %*% PAM
-  
+
   # richness and ranges
   ## richness (spp per cell) and ranges (ncells per sp)
   rich <- diag(A)
   rang <- diag(O)
-  
+
   ## richness and ranges adjusted to S and N
   richS <- rich / S
   rangN <- rang / N
-  
+
   ## traces of richness and ranges
   trA <- sum(rich)
   trO <- sum(rang)
-  
+
   # dispersal field and shared community composition
   ## values of dispersal field
-  if (any(indices %in% c("all", "DF"))) {
+  if (any(indices %in% c("all", "DF", "BL", "WRN", "SRC"))) {
     d_field <- c(PAM %*% rang)
     names(d_field) <- rownames(PAM)
     ## average
@@ -137,7 +139,7 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
   } else {
     d_field <- av_dfield <- NA
   }
-  
+
   ## values shared community composition
   if (any(indices %in% c("all", "SCC"))) {
     sc_comp <- c(tm1 %*% rich)
@@ -147,74 +149,75 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
   } else {
     sc_comp <- av_sccomp <- NA
   }
-  
+
   # other indices
   ## Whittaker's multiplicative beta
-  if (any(indices %in% c("all", "BW"))) {
+  if (any(indices %in% c("all", "BW", "WRN"))) {
     BW <- (S * N) / trO
   } else {
     BW <- NA
   }
-  
+
   ## Lande's additive beta
   if (any(indices %in% c("all", "AB"))) {
     BA <- S * (1 - (trO / (S * N)))
   } else {
+    BA <- NA
   }
-  
+
   ## Legendre's beta
   if (any(indices %in% c("all", "BL"))) {
     BL <- trO - sum(d_field)
   } else {
     BL <- NA
   }
-  
+
   ## Matrix of covariance of composition of sites
-  if (any(indices %in% c("all", "CMSC"))) {
+  if (any(indices %in% c("all", "CMSC", "SCSC"))) {
     CS_cov <- (A / S) - (richS %*% t(richS))
     ## Mean
     Ccov_mean <- (d_field / (N * S)) - (BW^-1 * richS)
   } else {
     CS_cov <- Ccov_mean <- NA
   }
-  
+
   ## Matrix of covariance of ranges of species
-  if (any(indices %in% c("all", "CMSR"))) {
+  if (any(indices %in% c("all", "CMSR", "SCSR"))) {
     RS_cov <- (O / S) - (rangN %*% t(rangN))
     ## Mean
     Rcov_mean <- (sc_comp / (N * S)) - (BW^-1 * rangN)
   } else {
     RS_cov <- Rcov_mean <- NA
   }
-  
+
   ## Schluter sites-composition covariance
   if (any(indices %in% c("all", "SCSC"))) {
     VCS_cov <- sum(CS_cov) / sum(diag(CS_cov))
   } else {
     VCS_cov <- NA
   }
-  
+
   ## Schluter species-ranges covariance
   if (any(indices %in% c("all", "SCSR"))) {
     VRS_cov <- sum(RS_cov) / sum(diag(RS_cov))
   } else {
     VRS_cov <- NA
   }
-  
+
   ## Wright & Reeves' nestedness
   if (any(indices %in% c("all", "WRN"))) {
     Nc <- (sum(d_field) - ((N * S) / BW)) / 2
   } else {
     Nc <- NA
   }
-  
+
   ## Stone & Roberts Cscore (Hadamard product (x) = element wise multiplication)
   if (any(indices %in% c("all", "SRC"))) {
     Cs <- (sum(O * O) - (N * av_dfield)) / 2
   } else {
     Cs <- NA
   }
-  
+
   # returning results
   tab_in <- data.frame(Value = c(N, S, av_dfield, av_sccomp, BA, BW, BL, VCS_cov,
                                  VRS_cov, Nc, Cs),
@@ -225,20 +228,20 @@ PAM_indices <- function(PAM, indices = "all", exclude_column = NULL) {
                                      "Schluter_cov_species_ranges",
                                      "Wright_Reeves_nestedness",
                                      "Stone_Roberts_Cscore"))
-  
+
   # if base_PAM
-  nil <- list(One_value_indices = tab_in, Richness = rich, Range = rang, 
-              Richness_standardized = richS, Range_standardized = rangN, 
+  nil <- list(One_value_indices = tab_in, Richness = rich, Range = rang,
+              Richness_standardized = richS, Range_standardized = rangN,
               Dispersal_field = d_field, Shared_community_composition = sc_comp,
               Cov_mat_sites_composition = CS_cov, Cov_mat_species_ranges = RS_cov)
-  
+
   if (cpam == "base_PAM") {
     if (is.null(bpam$PAM_indices)) {
       bpam$PAM_indices <- nil
     } else {
       bpam$PAM_indices <- refill_PAM_indices(bpam$PAM_indices, nil)
     }
-    
+
     return(bpam)
   } else {
     # if matrix
