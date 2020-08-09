81302fc86f794f2b613ff276dc8b208ff4df3749
claununez <claununez199o@gmail.com>
2020-08-08 22:26:48 -0500

    V 0.0.24 improved description and other helpers added

--- R/biosurvey.R
+++ R/biosurvey.R
@@ -1,8 +1,21 @@
 #' biosurvey: Tools for Biological Survey Planning
 #'
-#' biosurvey is a collection of tools that allow users to select sampling sites
-#' increasing efficiency of biodiversity survey systems by considering the
-#' relationship of environmental and geographic conditions in a region.
+#' biosurvey is a collection of tools that allow users to plan systems of sampling
+#' sites increasing the efficiency of biodiversity monitoring by considering the
+#' relationship between environmental and geographic conditions in a region.
+#' Three main modules are included: 1) Data preparation; 2) Selection of sets
+#' of sites for biodiversity sampling; and, 3) Tools for testing efficiency of
+#' distinct sets of sampling sites. Data are prepared ways that avoid the need
+#' for more data in posterior analyses, and allow concentrating in critical
+#' methodological decisions to select sampling sites. Various algorithms for
+#' selecting sampling sites are available, and options for considering
+#' pre-selected sites (known to be important for biodiversity monitoring) are
+#' included. Visualization is a critical component in this set of tools and most
+#' of the results obtained can be plotted to help to understand their implications.
+#' The options for selecting sampling sites include here differ from other
+#' implementations in that they consider the environmental and geographic
+#' structure of a region to suggest sampling sites that could increase the
+#' efficiency of efforts dedicated to monitoring biodiversity.
 #'
 #' @section Main functions in biosurvey:
 #' \code{\link{base_PAM}}, \code{\link{block_sample}},
@@ -17,14 +30,16 @@
 #'
 #' Other functions (important helpers)
 #'
-#' \code{\link{closest_to_centroid}}, \code{\link{files_2data}},
+#' \code{\link{assign_blocks}}, \code{\link{closest_to_centroid}},
+#' \code{\link{distance_filter}}, \code{\link{files_2data}},
 #' \code{\link{find_clusters}}, \code{\link{find_modes}},
 #' \code{\link{grid_from_region}}, \code{\link{match_rformat}},
 #' \code{\link{PAM_from_table}}, \code{\link{point_sample}},
 #' \code{\link{point_sample_cluster}}, \code{\link{point_thinning}},
-#' \code{\link{rlist_2data}}, \code{\link{selected_sites_PAM}},
-#' \code{\link{spdf_2data}}, \code{\link{stack_2data}},
-#' \code{\link{unimodal_test}}, \code{\link{wgs84_2aed_laea}}
+#' \code{\link{refill_PAM_indices}}, \code{\link{rlist_2data}},
+#' \code{\link{selected_sites_PAM}}, \code{\link{spdf_2data}},
+#' \code{\link{stack_2data}}, \code{\link{unimodal_test}},
+#' \code{\link{wgs84_2aed_laea}}
 #'
 #' @docType package
 #' @name biosurvey
