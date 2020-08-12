946647fd1808195f3ca3e31653d2ec0d27f4021c
claununez <claununez199o@gmail.com>
2020-08-11 21:12:45 -0500

    V 0.0.26 master classes using print and summary methods

--- R/Classes.R
+++ R/Classes.R
@@ -1,12 +1,12 @@
-new_master_matrix <- function(data_matrix, region, mask, raster_base,
-                              preselected_sites, PCA_results) {
-  stopifnot(is.data.frame(data_matrix))
 
+new_master_matrix <- function(data_matrix, preselected_sites = NULL, region,
+                              mask = NULL, raster_base, PCA_results = NULL) {
+  stopifnot(is.data.frame(data_matrix))
+  preclass <- class(preselected_sites)[1]
   reclass <- class(region)[1]
   mclass <- class(mask)[1]
   raclass <- class(raster_base)[1]
   pcaclass <- class(PCA_results)[1]
-  preclass <- class(preselected_sites)[1]
 
   if (!preclass %in% c("data.frame", "NULL")){
     stop("'preselected_sites' must be of class 'data.frame', or NULL.")
@@ -24,8 +24,45 @@ new_master_matrix <- function(data_matrix, region, mask, raster_base,
     stop("'PCA_results' must be of class 'prcomp', or NULL.")
   }
 
-  return(structure(list(data_matrix = data_matrix, region = region, mask = mask,
-                        raster_base = raster_base,
-                        preselected_sites = preselected_sites,
-                        PCA_results = PCA_results), class = "master_matrix"))
+  val <- list(data_matrix = data_matrix, preselected_sites = preselected_sites,
+              region = region, mask = mask, raster_base = raster_base,
+              PCA_results = PCA_results)
+  class(val) <- "master_matrix"
+  return(val)
+}
+
+
+new_master_selection <- function(data_matrix, preselected_sites = NULL, region,
+                                 mask = NULL, raster_base, PCA_results = NULL,
+                                 selected_sites_random = NULL, selected_sites_G = NULL,
+                                 selected_sites_E = NULL, selected_sites_EG = NULL) {
+
+  randclass <- class(selected_sites_random)[1]
+  gclass <- class(selected_sites_G)[1]
+  eclass <- class(selected_sites_E)[1]
+  egclass <- class(selected_sites_EG)[1]
+
+  if (!randclass %in% c("list", "NULL")) {
+    stop("'selected_sites_random' must be of class 'list', or NULL.")
+  }
+  if (!gclass %in% c("list", "NULL")) {
+    stop("'selected_sites_G' must be of class 'list', or NULL.")
+  }
+  if (!eclass %in% c("list", "NULL")) {
+    stop("'selected_sites_E' must be of class 'list', or NULL.")
+  }
+  if (!egclass %in% c("list", "NULL")) {
+    stop("'selected_sites_EG' must be of class 'list', or NULL.")
+  }
+
+  val <- new_master_matrix(data_matrix = data_matrix,
+                           preselected_sites = preselected_sites, region = region,
+                           mask = mask, raster_base = raster_base,
+                           PCA_results = PCA_results)
+  val <- c(val, list(selected_sites_random = selected_sites_random,
+                     selected_sites_G = selected_sites_G,
+                     selected_sites_E = selected_sites_E,
+                     selected_sites_EG = selected_sites_EG))
+  class(val) <- c("master_selection", "master_matrix")
+  return(val)
 }
--- R/Methods.R
+++ R/Methods.R
@@ -1,19 +1,121 @@
+#' Print a short version of elements in master objects
+#' @name print
+#' @aliases print,master_matrix-method print,master_selection-method
+#' @param object object of class master_matrix or master_selection.
+#' @export
+#' @rdname print
 
-#print.master_matrix <- function(x) {
-#  cat("A master_matrix object:")
-#
-#}
+print.master_matrix <- function(object) {
+  cat("data_matrix:\n")
+  print(head(object$data_matrix))
+
+  cat("\npreselected_sites:\n")
+  if (!is.null(object$preselected_sites)) {
+    print(head(object$preselected_sites))
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nregion:\n")
+  print(object$region)
+
+  cat("\nmask:\n")
+  if (!is.null(object$mask)) {
+    print(object$mask)
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nraster_base:\n")
+  print(object$raster_base)
+
+  cat("\nPCA_results:\n")
+  if (!is.null(object$PCA_results)) {
+    print(object$PCA_results)
+  } else {
+    cat("Empty\n")
+  }
+}
+
+
+#' @export
+#' @rdname print
+
+print.master_selection <- function(object) {
+  cat("data_matrix:\n")
+  print(head(object$data_matrix))
+
+  cat("\npreselected_sites:\n")
+  if (!is.null(object$preselected_sites)) {
+    print(head(object$preselected_sites))
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nregion:\n")
+  print(object$region)
+
+  cat("\nmask:\n")
+  if (!is.null(object$mask)) {
+    print(mask)
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nraster_base:\n")
+  print(object$raster_base)
+
+  cat("\nPCA_results:\n")
+  if (!is.null(object$PCA_results)) {
+    print(object$PCA_results)
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nselected_sites_random:\n")
+  if (!is.null(object$selected_sites_random)) {
+    cat("First of", length(object$selected_sites_random), "element(s).\n")
+    print(head(object$selected_sites_random[[1]]))
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nselected_sites_G:\n")
+  if (!is.null(object$selected_sites_G)) {
+    cat("First of", length(object$selected_sites_G), "element(s).\n")
+    print(head(object$selected_sites_G[[1]]))
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nselected_sites_E:\n")
+  if (!is.null(object$selected_sites_E)) {
+    cat("First of", length(object$selected_sites_E), "element(s).\n")
+    print(head(object$selected_sites_E[[1]]))
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\nselected_sites_EG:\n")
+  if (!is.null(object$selected_sites_EG)) {
+    cat("First of", length(object$selected_sites_EG), "element(s).\n")
+    print(head(object$selected_sites_EG[[1]]))
+  } else {
+    cat("Empty\n")
+  }
+}
+
+
 
 
 #' Summary of attributes and results
 #' @name summary
-#' @aliases summary,master_matrix-method
-#' @param object object of class master_matrix.
+#' @aliases summary,master_matrix-method summary,master_selection-method
+#' @param object object of class master_matrix or master_selection.
 #' @export
 #' @return
 #' A printed summary.
 #' @rdname summary
-
 
 summary.master_matrix <- function(object) {
   # -----------
@@ -24,26 +126,79 @@ summary.master_matrix <- function(object) {
       stop("Argument 'object' must be of class 'master_matrix'")
     }
   }else {
-    stop("Argument 'master_matrix' is necessary.")
+    stop("Argument 'object' is necessary.")
   }
 
   cat("\n                     Summary of a master_matrix object\n")
   cat("---------------------------------------------------------------------------\n\n")
-  cat("Data matrix summary:\n\n")
+  cat("Data matrix summary:\n")
   print(summary(object$data_matrix))
   if (!is.null(object$preselected_sites)) {
-    cat("\n\nSites preselected by user:\n\n")
+    cat("\n\nSites preselected by user:\n")
     print(object$preselected_sites[, 1:3])
+  } else {
+    cat("\n\nNo preselected sites were defined\n")
   }
-  cat("\n\nRegion of interest:\n\n")
+  cat("\n\nRegion of interest:\n")
   print(object$region)
   if (!is.null(object$mask)) {
-    cat("\n\nMask to area of interest:\n\n")
+    cat("\n\nMask to area of interest:\n")
     print(object$mask)
-  }
-  if (!is.null(object$PCA_results)) {
-    cat("\n\nPCA results:\n\n")
-    print(object$PCA_results)
   }
 }
 
+#' @export
+#' @rdname summary
+
+summary.master_selection <- function(object, nrow = 6, ncol = 2) {
+  # -----------
+  # detecting potential errors
+  if (!missing(object)) {
+    clo <- class(object)[1]
+    if (clo != "master_selection") {
+      stop("Argument 'object' must be of class 'master_selection'")
+    }
+  }else {
+    stop("Argument 'object' is necessary.")
+  }
+
+  cat("\n                  Summary of a master_selection object\n")
+  cat("---------------------------------------------------------------------------\n\n")
+  if (!is.null(object$preselected_sites)) {
+    cat("Sites preselected by user:\n")
+    print(object$preselected_sites[, 1:3])
+  } else {
+    cat("No preselected sites were defined\n")
+  }
+
+  cat("\n\n")
+  cat(ncol, "columns and ", nrow, "rows of first elements are shown")
+
+  cat("\n\nSites selected randomly:\n")
+  if (!is.null(object$selected_sites_random)) {
+    print(object$selected_sites_random[[1]][1:nrow, 1:ncol])
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\n\nSites selected uniformly in G space:\n")
+  if (!is.null(object$selected_sites_G)) {
+    print(object$selected_sites_G[[1]][1:nrow, 1:ncol])
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\n\nSites selected uniformly in E space:\n")
+  if (!is.null(object$selected_sites_E)) {
+    print(object$selected_sites_E[[1]][1:nrow, 1:ncol])
+  } else {
+    cat("Empty\n")
+  }
+
+  cat("\n\nSites selected uniformly in E space, considering G structure:\n")
+  if (!is.null(object$selected_sites_EG)) {
+    print(object$selected_sites_EG[[1]][1:nrow, 1:ncol])
+  } else {
+    cat("Empty\n")
+  }
+}
