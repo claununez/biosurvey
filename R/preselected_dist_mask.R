ea54d90675f2a0ff67b78c2a4aa45b1bad6df0e4
claununez <claununez199o@gmail.com>
2020-08-16 23:47:45 -0500

    V 0.0.29 documentation added to preselected_dist_mask

--- R/preselected_dist_mask.R
+++ R/preselected_dist_mask.R
@@ -1,5 +1,48 @@
+#' Helper to create objects to detect points to close to preselected sites
+#'
+#' @param master a master_matrix object derived from function
+#' \code{\link{prepare_master_matrix}} or a master_selection object derived from functions
+#' \code{\link{random_selection}}, \code{\link{uniformE_selection}},
+#' or \code{\link{EG_selection}}.
+#' @param expected_points (numeric) number of survey points (sites) to be selected.
+#' @param space (character) space in which the thinning will be performed. There
+#' are two options available: "G", if it will be in geographic space, and
+#' "E", if it will be in environmental space.
+#' @param variable_1 (character or numeric) name or position of the first
+#' variable (X-axis). Default = NULL.
+#' @param variable_2 (character or numeric) name or position of the second
+#' variable (Y-axis). Default = NULL.
+#' @param use_blocks (logical) whether or not to use block centroids instead of
+#' all points when \code{space} = "E". Default = FALSE.
+#' @param verbose (logical) whether or not to print messages about the process.
+#' Default = TRUE.
+#'
+#' @return
+#' A list of two elements: the distance used to obtain \code{expected_points} and
+#' a SpatialPolygonsDataFrame object created from preselected_sites in master.
+#'
+#' @usage
+#' preselected_dist_mask(master, expected_points, space, variable_1 = NULL,
+#'                       variable_2 = NULL, use_blocks = FALSE,
+#'                       verbose = TRUE)
+#'
+#' @export
+#' @importFrom raster pointDistance disaggregate
+#' @importFrom sp SpatialPolygonsDataFrame spTransform SpatialPointsDataFrame
+#' @importFrom rgeos gBuffer
+#' @importFrom stats dist
+#'
+#' @examples
+#' # data
+#' data("m_matrix_pre", package = "biosurvey")
+#'
+#' # running
+#' pdm <- preselected_dist_mask(master = m_matrix_pre, expected_points = 20,
+#'                              space = "G")
+
 preselected_dist_mask <- function(master, expected_points, space, variable_1 = NULL,
-                                  variable_2 = NULL, use_blocks = FALSE) {
+                                  variable_2 = NULL, use_blocks = FALSE,
+                                  verbose = TRUE) {
 
   # Initial tests
   if (missing(master)) {
@@ -38,7 +81,9 @@ preselected_dist_mask <- function(master, expected_points, space, variable_1 = N
     stop(mess, " is smaller than 'expected_points'.")
   }
   if (np == expected_points) {
-    message("Number of points in 'data_matrix' equals 'expected_points'.")
+    if (verbose == TRUE) {
+      message("Number of points in 'data_matrix' equals 'expected_points'.")
+    }
   }
 
   # Processing
@@ -46,9 +91,6 @@ preselected_dist_mask <- function(master, expected_points, space, variable_1 = N
     x_column <- "Longitude"
     y_column <- "Latitude"
     gv <- c(x_column, y_column)
-
-    alld <- apply(raster::pointDistance(data[, gv], pre[, gv], lonlat = TRUE),
-                  1, min)
 
     ext <- apply(data[, gv], 2, range)
     mxdis <- raster::pointDistance(ext[1, ], ext[2, ], lonlat = TRUE)
@@ -58,12 +100,6 @@ preselected_dist_mask <- function(master, expected_points, space, variable_1 = N
     x_column <- variable_1
     y_column <- variable_2
     gv <- c(x_column, y_column)
-
-    ep <- t(data[, gv])
-    pres <- t(pre[, gv])
-
-    alld <- apply(apply(pres, 2, function(pres) {sqrt(colSums((ep - pres)^2))}),
-                  1, min)
 
     ext <- apply(data[, gv], 2, range)
     dist <- c(stats::dist(ext)) / expected_points
@@ -76,7 +112,9 @@ preselected_dist_mask <- function(master, expected_points, space, variable_1 = N
   count <- 1
 
   # testing distance
-  message("Running distance optimization, please wait...")
+  if (verbose == TRUE) {
+    message("Running distance optimization, please wait...")
+  }
 
   while (np > expected_points) {
     # thinning
@@ -107,10 +145,21 @@ preselected_dist_mask <- function(master, expected_points, space, variable_1 = N
   }
 
   # distance determined, creating mask
-  sppre <- wgs84_2aed_laea(pre, x_column, y_column)
-
-  maskp <- rgeos::gBuffer(sppre, width = dist * 1000, quadsegs = 100)
-  maskp <- sp::spTransform(maskp, sp::CRS("+init=epsg:4326"))
+  if (space == "G") {
+    sppre <- wgs84_2aed_laea(pre, x_column, y_column)
+    maskp <- rgeos::gBuffer(sppre, width = dist * 1000, quadsegs = 100)
+    maskp <- raster::disaggregate(maskp)
+    maskp <- sp::SpatialPolygonsDataFrame(maskp, data.frame(ID = 1:length(maskp)),
+                                          match.ID = FALSE)
+    maskp <- sp::spTransform(maskp, sp::CRS("+init=epsg:4326"))
+  } else {
+    sppre <- sp::SpatialPointsDataFrame(pre[, c(x_column, y_column)], pre,
+                                        proj4string = sp::CRS("+init=epsg:4326"))
+    maskp <- suppressWarnings(rgeos::gBuffer(sppre, width = dist, quadsegs = 100))
+    maskp <- raster::disaggregate(maskp)
+    maskp <- sp::SpatialPolygonsDataFrame(maskp, data.frame(ID = 1:length(maskp)),
+                                          match.ID = FALSE)
+  }
 
   # returning results
   return(list(distance = dist, mask = maskp))
