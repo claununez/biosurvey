714386b2b0ad60244aa476f4e15083834a938430
claununez <claununez199o@gmail.com>
2020-08-17 00:12:23 -0500

    V 0.0.30 functional version of selection in G including preselected sites

--- R/uniformG_selection.R
+++ R/uniformG_selection.R
@@ -8,25 +8,27 @@
 #' \code{\link{random_selection}}, \code{\link{uniformE_selection}},
 #' or \code{\link{EG_selection}}.
 #' @param expected_points (numeric) number of survey points (sites) to be selected.
+#' @param guess_distances (logical) whether or not to use internal algorithm
+#' to automatically select \code{initial_distance} and \code{increase}. Default
+#' = TRUE. If FALSE, \code{initial_distance} and \code{increase} must be defined.
+#' @param initial_distance (numeric) distance in km to be used for a first
+#' process of thinning and detection of remaining points. Default = NULL.
+#' @param increase (numeric) value to be added to \code{initial_distance} until
+#' reaching the number of \code{expected_points}. Default = NULL.
 #' @param max_n_samplings (numeric) maximum number of samples to be chosen after
 #' performing all thinning \code{replicates}. Default = 1.
-#' @param initial_distance (numeric) distance in km to be used for a first
-#' process of thinning and detection of remaining points.
-#' @param increase (numeric) value to be added to \code{initial_distance} until
-#' reaching the number of \code{expected_points}.
 #' @param replicates (numeric) number of thinning replicates. Default = 10.
 #' @param use_preselected_sites (logical) whether to use sites that have been
 #' defined as part of the selected sites previous any selection. Object in
 #' \code{master} must contain the site(s) preselected in and element of name
 #' "preselected_sites" for this argument to be effective. Default = TRUE.
 #' See details for more information on the approach used.
-#' @param n_optimization (numeric) number of times the algorithm of optimization
-#' for site selection when using \code{use_preselected_sites} will run. Default
-#' = 1000.
 #' @param median_distance_filter (character) optional argument to define a median
 #' distance-based filter based on which sets of sampling sites will be selected.
 #' The default, NULL, does not apply such a filter. Options are: "max" and "min".
 #' @param set_seed (numeric) integer value to specify a initial seed. Default = 1.
+#' @param verbose (logical) whether or not to print messages about the process.
+#' Default = TRUE.
 #'
 #' @return
 #' A master_selection object (S3) with an additional element called
@@ -48,9 +50,10 @@
 #' If \code{use_preselected_sites} is TRUE and such sites are included as an
 #' element in the object in \code{master}, the approach for selecting uniform
 #' sites in geography is different than what was described above. User preselected
-#' sites will always be part of the sites selected and other points are selected
-#' based on an process of optimization that searches for the random set of points
-#' that has the largest median distance among \code{n_optimization} distinct sets.
+#' sites will always be part of the sites selected. Other points are selected
+#' based on an process that searches for sites that are uniformly distributed
+#' in geographic space but at a distance from preselected sites that helps in
+#' maintaining such uniformity.
 #'
 #' As multiple sets could result from selection when the \code{use_preselected_sites}
 #' is set as FALSE, the argument of the function \code{median_distance_filter}
@@ -66,38 +69,45 @@
 #' \code{\link{EG_selection}}, \code{\link{plot_sites_EG}}
 #'
 #' @usage
-#' uniformG_selection(master, expected_points, max_n_samplings = 1,
-#'                    initial_distance, increase, replicates = 10,
+#' uniformG_selection(master, expected_points, guess_distances = TRUE,
+#'                    initial_distance = NULL, increase = NULL,
+#'                    max_n_samplings = 1, replicates = 10,
 #'                    use_preselected_sites = TRUE, n_optimization = 1000,
-#'                    median_distance_filter = NULL, set_seed = 1)
+#'                    median_distance_filter = NULL, set_seed = 1,
+#'                    verbose = TRUE)
 #'
 #' @export
+#' @importFrom sp over SpatialPointsDataFrame CRS
 #'
 #' @examples
 #' # Data
 #' data("m_matrix", package = "biosurvey")
 #'
 #' # Selecting sites uniformly in G space
-#' selectionG <- uniformG_selection(m_matrix, expected_points = 40, max_n_samplings = 1,
-#'                                  initial_distance = 145, increase = 1,
-#'                                  replicates = 5, set_seed = 1)
+#' selectionG <- uniformG_selection(m_matrix, expected_points = 40,
+#'                                  max_n_samplings = 1, initial_distance = 145,
+#'                                  increase = 1, replicates = 5, set_seed = 1)
 
-uniformG_selection <- function(master, expected_points, max_n_samplings = 1,
-                               initial_distance, increase, replicates = 10,
+uniformG_selection <- function(master, expected_points, guess_distances = TRUE,
+                               initial_distance = NULL, increase = NULL,
+                               max_n_samplings = 1, replicates = 10,
                                use_preselected_sites = TRUE, n_optimization = 1000,
-                               median_distance_filter = NULL, set_seed = 1) {
+                               median_distance_filter = NULL, set_seed = 1,
+                               verbose = TRUE) {
   # initial tests
   if (missing(master)) {
     stop("Argument 'master' is not defined.")
   }
-  if (missing(initial_distance)) {
-    stop("Argument 'initial_distance' is not defined.")
-  }
-  if (missing(increase)) {
-    stop("Argument 'increase' is not defined.")
-  }
   if (missing(expected_points)) {
     stop("Argument 'expected_points' is not defined.")
+  }
+  if (guess_distances == FALSE) {
+    if (missing(initial_distance)) {
+      stop("Argument 'initial_distance' is not defined.")
+    }
+    if (missing(increase)) {
+      stop("Argument 'increase' is not defined.")
+    }
   }
   if (!is.null(median_distance_filter)) {
     if (!median_distance_filter %in% c("max", "min")) {
@@ -105,7 +115,9 @@ uniformG_selection <- function(master, expected_points, max_n_samplings = 1,
     }
   }
   if (use_preselected_sites == TRUE & is.null(master$preselected_sites)) {
-    message("Element 'preselected_sites' in 'master' is NULL, setting\n'use_preselected_sites' = FALSE.")
+    if (verbose == TRUE) {
+      message("Element 'preselected_sites' in 'master' is NULL, setting\n'use_preselected_sites' = FALSE.")
+    }
     use_preselected_sites <- FALSE
   }
 
@@ -113,115 +125,126 @@ uniformG_selection <- function(master, expected_points, max_n_samplings = 1,
   data <- master$data_matrix
   x_column <- "Longitude"
   y_column <- "Latitude"
-  data <- data[!is.na(data[, x_column]) & !is.na(data[, y_column]), ]
 
   # selection depending on option of user points
   if (use_preselected_sites == TRUE) {
-    # using preselected sites, optimization based on random selection
-    n <- nrow(data)
-    pre <- master$preselected_sites
+    # using preselected sites to create mask and define distance
+    tst <- preselected_dist_mask(master, expected_points = expected_points,
+                                 space = "G", verbose = verbose)
 
-    selected_sites <- lapply(1:n_optimization, function(x) {
-      set.seed(set_seed + x - 1)
-      sam <- sample(n, expected_points)
-      dat <- unique(rbind(pre[, -1], data[sam, ]))
-      dat[1:expected_points, ]
-    })
+    # excluding close points from analysis
+    data <- sp::SpatialPointsDataFrame(data[, c("Longitude", "Latitude")], data,
+                                        proj4string = sp::CRS("+init=epsg:4326"))
+    data <- data[is.na(sp::over(data, tst$mask)$ID), ]@data
 
-    # Post filtering to get higher uniformity in G
-    selected_sites <- distance_filter(selected_sites, "max")[1]
-
-    # Returning results
-    names(selected_sites) <- paste0("selection_", 1:length(selected_sites))
-
-    if (class(master)[1] == "master_matrix") {
-      return(new_master_selection(master$data_matrix, master$preselected_sites,
-                                  master$region, master$mask, master$raster_base,
-                                  master$PCA_results, selected_sites_random = NULL,
-                                  selected_sites, selected_sites_E = NULL,
-                                  selected_sites_EG = NULL))
-
-    } else {
-      return(new_master_selection(master$data_matrix, master$preselected_sites,
-                                  master$region, master$mask, master$raster_base,
-                                  master$PCA_results, master$selected_sites_random,
-                                  selected_sites, master$selected_sites_E,
-                                  master$selected_sites_EG))
-    }
-
+    # npre
+    npre <- nrow(master$preselected_sites)
+    expected_points <- expected_points - npre
   } else {
-    # preparing selection variables
-    np <- nrow(data)
-    ininp <- np
+    npre <- 0
+  }
+
+  # preparing selection variables
+  if (guess_distances == TRUE) {
+    dist <- round(tst$distance, 2)
+    increase <- dist / 10
+  } else {
     dist <- initial_distance
-    inin <- 1
-    count <- 1
+  }
 
-    # condition
-    if (np < expected_points) {
-      stop("Number of points in 'master_matrix' is smaller than 'expected_points'.")
+  np <- nrow(data)
+  ininp <- np
+  pnp <- np
+  inin <- 1
+  count <- 1
+
+  # condition
+  if (np < expected_points) {
+    stop("Number of available points in is smaller than 'expected_points'.")
+  }
+  if (np == expected_points) {
+    if (verbose == TRUE) {
+      message("Number of available points equals 'expected_points'.",
+              "\nReturning all availale points as selected sites.")
     }
-    if (np == expected_points) {
-      message("Number of points in 'master_matrix' equals 'expected_points'.")
 
-      return(new_master_selection(master$data_matrix, master$preselected_sites,
-                                  master$region, master$mask, master$raster_base,
-                                  master$PCA_results, master$selected_sites_random,
-                                  list(selection_1 = data), master$selected_sites_E,
-                                  master$selected_sites_EG))
+    if (use_preselected_sites == TRUE) {
+      data <- rbind(master$preselected_sites[, -1],
+                    data[, colnames(data) != "Selected_blocks"])
     }
 
-    # selection process
-    while (np > expected_points) {
-      # thinning
-      thin <- point_thinning(data, x_column, y_column, dist, space = "G",
-                             max_n_samplings, replicates, set_seed)
-      np <- nrow(thin[[1]])
+    return(new_master_selection(master$data_matrix, master$preselected_sites,
+                                master$region, master$mask, master$raster_base,
+                                master$PCA_results, master$selected_sites_random,
+                                list(selection_1 = data), master$selected_sites_E,
+                                master$selected_sites_EG))
+  }
+
+  # selection process
+  if (verbose == TRUE) {
+    message("Running algorithm for selecting sites, please wait...")
+  }
+
+  while (np > expected_points) {
+    # thinning
+    thin <- point_thinning(data, x_column, y_column, dist, space = "G",
+                           max_n_samplings, replicates, set_seed)
+    np <- nrow(thin[[1]])
+    if (verbose == TRUE) {
       message("    Distance  ", dist, "  resulted in  ", np, "  points")
+    }
 
-      if (np <= expected_points) {
-        if (np == expected_points) {
-          # success
-          if (length(thin) > 1 & !is.null(median_distance_filter)) {
-            thin <- distance_filter(thin, median_distance_filter)
+    if (np <= expected_points) {
+      if (np == expected_points) {
+        # selection done
+        break()
+      } else {
+        # reducing initial distance
+        dist <- dist - increase
 
-          }
-          names(thin) <- paste0("selection_", 1:length(thin))
-
-          return(new_master_selection(master$data_matrix, master$preselected_sites,
-                                      master$region, master$mask, master$raster_base,
-                                      master$PCA_results, master$selected_sites_random,
-                                      thin, master$selected_sites_E,
-                                      master$selected_sites_EG))
-
-        } else {
-          if (count == 1) {
-            stop("'initial_distance' resulted in  ", np, "  points. Try smaller values.")
-          } else {
-            # reducing increase distance
-            message("\nNo distance resulted in  ", expected_points, "  points.",
-                    "\nTrying distances between  ", pdist, "  and  ", dist, ".\n")
-            dist <- pdist
-
-            increase <- increase / 10
-            inin <- inin + 1
-
-            if (inin == 3) {
-              stop("\nNo distance resulted in  ", expected_points,
-                   "  points after trying smaller intervals.",
-                   "\nTry a distance of  ", pdist,
-                   "  with values for 'increase' below  ", increase, ".")
-            }
-
-            np <- ininp
-          }
+        # reducing increase distance
+        if (count > 1 & pnp > expected_points) {
+          increase <- increase / 10
         }
       }
-
-      # starting again
-      count <- count + 1
-      pdist <- dist
+    } else {
       dist <- dist + increase
     }
+
+    # starting again
+    pnp <- np
+    np <- ininp
+    count <- count + 1
+  }
+
+  # Returning results
+  ## adding preselected to selected
+  if (use_preselected_sites == TRUE) {
+    thin <- lapply(thin, function(x) {
+      rbind(master$preselected_sites[, -1], x[, colnames(x) != "Selected_blocks"])
+    })
+  }
+
+  ## filtering by distances success
+  if (length(thin) > 1 & !is.null(median_distance_filter)) {
+    thin <- distance_filter(thin, median_distance_filter)
+  }
+
+  ## naming and returning
+  names(thin) <- paste0("selection_", 1:length(thin))
+
+  if (class(master)[1] == "master_matrix") {
+    return(new_master_selection(master$data_matrix, master$preselected_sites,
+                                master$region, master$mask, master$raster_base,
+                                master$PCA_results, selected_sites_random = NULL,
+                                thin, selected_sites_E = NULL,
+                                selected_sites_EG = NULL))
+
+  } else {
+    return(new_master_selection(master$data_matrix, master$preselected_sites,
+                                master$region, master$mask, master$raster_base,
+                                master$PCA_results, master$selected_sites_random,
+                                thin, master$selected_sites_E,
+                                master$selected_sites_EG))
   }
 }
