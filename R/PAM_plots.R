742908d823a620c3c16f4c800dccc4d686818d3f
claununez <claununez199o@gmail.com>
2020-08-18 23:35:08 -0500

    V 0.0.33 plot PAM in geography added and documented

---
+++ R/PAM_plots.R
@@ -0,0 +1,96 @@
+#' Plot of PAM indices in geography
+#'
+#' @param PAM an object of class base_PAM.
+#' @param index (character) code for the index to be plotted. Options are: "RI",
+#' "RIN", "DF", or "MCC". Default = "RI"
+#' @param col_pal color palette function to be used in defining colors for the
+#' \code{index} to be plotted. The default, NULL, uses \code{viridis::viridis}.
+#' @param border color for cell borders of the PAM grid. The default, NULL, does
+#' not plot any border.
+#' @param cex (numeric) value by which plotting elements should be magnified
+#' relative to the default. Default = 0.9
+#'
+#' @return
+#' A plot of \code{index} represented in geography.
+#'
+#' @usage
+#' plot_PAM_geo(PAM, index = "RI", col_pal = NULL, border = NULL, cex = 0.9)
+#'
+#' @export
+#' @importFrom sp plot
+#' @importFrom graphics layout par plot.new
+#' @importFrom maps map
+#'
+#' @examples
+#' data("b_pam", package = "biosurvey")
+#'
+#' plot_PAM_geo(b_pam, index = "RI")
+
+plot_PAM_geo <- function(PAM, index = "RI", col_pal = NULL, border = NULL,
+                         cex = 0.9) {
+  if (missing(PAM)) {
+    stop("Argument 'PAM' is missing.")
+  }
+  all_in <- c("RI", "RIN", "DF", "MCC")
+  if (!index %in% all_in) {
+    stop("Argument 'index' is not valid, options are: 'RI', 'RIN', 'DF', or 'MCC'.")
+  }
+
+  # index selection
+  PAM <- PAM_indices(PAM, indices = "all")
+  g_indices <- names(PAM$PAM_indices)[-c(1, 3, 5, 7, 9:11)]
+  names(g_indices) <- all_in
+
+  # color definition
+  if (is.null(col_pal)) {
+    col_pal <- viridis::viridis
+  }
+  if (is.null(border)) {
+    border <- NA
+  }
+
+  rfactor <- range(PAM$PAM_indices[[g_indices[index]]])
+  ifactor <- as.factor(PAM$PAM_indices[[g_indices[index]]])
+  n <- length(levels(ifactor))
+  col <- col_pal(n)
+
+  # par settings
+  opar <- par(no.readonly = TRUE)
+  on.exit(par(opar))
+
+  # plotting
+  layout(matrix(1:2, 1, byrow = T), widths = c(10, 1.5))
+  par(cex = cex, mar = rep(0, 4))
+
+  sp::plot(PAM$PAM, border = "transparent")
+  maps::map(fill = TRUE, col = "gray97", lforce = "n",
+            border = "gray80", add = TRUE)
+  sp::plot(PAM$PAM, col = col[ifactor], border = border, add = TRUE)
+  box()
+  plot.new()
+  bar_legend(rfactor, col = col, title = gsub("_", " ", g_indices[index]),
+             round = 3, label_x = 0.5, labels_y = c(0.18, 0.87))
+}
+
+
+
+
+
+plot_PAM_KS <- function(PAM, exclude_column = NULL) {
+  if (!class(PAM)[1] %in% c("base_PAM", "matrix", "data.frame")) {
+    stop("Class of 'PAM' is not supported, see function's help.")
+  }
+
+  # preparing data
+  if (class(PAM)[1] == "base_PAM") {
+    PAM <- PAM_indices(PAM, indices = "all")
+    x <- PAM$PAM_indices$Richness_normalized
+    y <- PAM$PAM_indices$Dispersion_field
+  } else {
+    PAM <- PAM_indices(PAM, indices = "all", exclude_column = exclude_column)
+    x <- PAM$Richness_normalized
+    y <- PAM$Dispersion_field
+
+  }
+
+}
