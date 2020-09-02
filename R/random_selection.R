3c903513c0714e3fd776e649dfa70457b5f02068
claununez <claununez199o@gmail.com>
2020-09-02 01:09:56 -0500

    V 0.0.39 improved message false preselected

--- R/random_selection.R
+++ R/random_selection.R
@@ -80,7 +80,7 @@ random_selection <- function(master, n_sites, n_samplings = 1,
     }
   }
   if (use_preselected_sites == TRUE & is.null(master$preselected_sites)) {
-    message("Element 'preselected_sites' in 'master' is NULL, such sites were not used.")
+    message("Element 'preselected_sites' in 'master' is NULL, setting\n'use_preselected_sites' = FALSE")
     use_preselected_sites <- FALSE
   }
 
