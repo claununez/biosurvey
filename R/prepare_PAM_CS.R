e8d5aa372e8514e8860bd98e5c0aed05cba5d73a
marlonecobos <manubio13@gmail.com>
2021-02-23 02:49:49 -0600

    V 0.0.49 inclreasing Sys.sleep in progress bar

--- R/prepare_PAM_CS.R
+++ R/prepare_PAM_CS.R
@@ -155,7 +155,7 @@ prepare_PAM_CS <- function(PAM, exclude_column = NULL, id_column = NULL,
           count <<- count + length(list(...)) - 1
           utils::setTxtProgressBar(pb, count)
           utils::flush.console()
-          Sys.sleep(0.01)
+          Sys.sleep(0.1)
           cbind(...)
         }
       }
