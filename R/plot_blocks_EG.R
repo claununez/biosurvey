dd889c921dfd1dc6928b6cc1935a7d5e76859ed8
marlonecobos <manubio13@gmail.com>
2021-03-14 23:53:36 -0500

    V 0.0.51 plot blocks separate functions

--- R/plot_blocks_EG.R
+++ R/plot_blocks_EG.R
@@ -80,6 +80,8 @@
 #'
 #' plot_blocks_EG(master = m_blocks, variable_1 = "PC1", variable_2 = "PC2",
 #'                block_ID = TRUE)
+#' plot_blocks_E(master = m_blocks, variable_1 = "PC1", variable_2 = "PC2")
+#' plot_blocks_G(master = m_blocks)
 #'
 #' # Defining your own colors
 #' n_blocks <- length(m_blocks$data_matrix$Block)
@@ -96,6 +98,70 @@ plot_blocks_EG <- function(master, variable_1, variable_2, region_border = TRUE,
                            col_all = NULL, col_selected = NULL, col_ID = NULL,
                            cex_all = 0.7, cex_selected = 1, cex_ID = 1,
                            pch_all = 16, pch_selected = 16, add_main = TRUE) {
+  # initial tests
+  if (missing(master)) {
+    stop("Argument 'master' is required to produce the plot.")
+  }
+  if (missing(variable_1)) {
+    stop("Argument 'variable_1' is required to produce the plot.")
+  }
+  if (missing(variable_2)) {
+    stop("Argument 'variable_2' is required to produce the plot.")
+  }
+  if (!class(master)[1] %in% c("master_matrix", "master_selection")) {
+    stop("Object defined in 'master' is not valid, see function's help.")
+  }
+
+  ## par settings
+  opar <- par(no.readonly = TRUE)
+  on.exit(par(opar))
+
+  # plot
+  ## main-layout
+  if (add_main == TRUE) {
+    layout(matrix(1:4, 2, byrow = T), widths = c(10, 10), heights = c(1, 10))
+    par(mar = rep(0, 4), cex = 0.7)
+
+    plot.new()
+    text(0.5, 0.5, "Environmental space", cex = 1.3)
+    plot.new()
+    text(0.5, 0.5, "Geographic space", cex = 1.3)
+
+    par(mar = c(3.5, 3.5, 0.5, 1))
+  } else {
+    if (all.equal(par()$mfrow, c(1, 1)) == TRUE) {
+      par(mfrow = c(1, 2))
+    }
+    par(mar = c(3.5, 3.5, 0.5, 1), cex = 0.7)
+  }
+
+  ## environmental space
+  plot_blocks_E(master, variable_1, variable_2, which, block_ID, col_all,
+                col_selected, col_ID, cex_all, cex_selected, cex_ID,
+                pch_all, pch_selected)
+
+  ## geographic space
+  par(mar = rep(0.5, 4))
+  plot_blocks_G(master, region_border, mask_border, which, block_ID, col_all,
+                col_selected, col_ID, cex_all, cex_selected, cex_ID, pch_all,
+                pch_selected)
+}
+
+
+#' @rdname plot_blocks_EG
+#' @export
+#' @param main (character) the main title for the plot.
+#' @usage
+#' plot_blocks_E(master, variable_1, variable_2, which = "all",
+#'               block_ID = FALSE, col_all = NULL, col_selected = NULL,
+#'               col_ID = NULL, cex_all = 0.7, cex_selected = 1,
+#'               cex_ID = 1, pch_all = 16, pch_selected = 16, main = "")
+
+plot_blocks_E <- function(master, variable_1, variable_2, which = "all",
+                          block_ID = FALSE, col_all = NULL, col_selected = NULL,
+                          col_ID = NULL, cex_all = 0.7, cex_selected = 1,
+                          cex_ID = 1, pch_all = 16, pch_selected = 16,
+                          main = "") {
   # initial tests
   if (missing(master)) {
     stop("Argument 'master' is required to produce the plot.")
@@ -126,7 +192,6 @@ plot_blocks_EG <- function(master, variable_1, variable_2, region_border = TRUE,
   ublocks <- unique(master$data_matrix$Block)
   nblocks <- length(ublocks)
 
-  gvars <- c("Longitude", "Latitude")
   evars <- c(variable_1, variable_2)
 
   # colors
@@ -159,32 +224,9 @@ plot_blocks_EG <- function(master, variable_1, variable_2, region_border = TRUE,
     }
   }
 
-  ## par settings
-  opar <- par(no.readonly = TRUE)
-  on.exit(par(opar))
-
-  # plot
-  ## main-layout
-  if (add_main == TRUE) {
-    layout(matrix(1:4, 2, byrow = T), widths = c(10, 10), heights = c(1, 10))
-    par(mar = rep(0, 4), cex = 0.7)
-
-    plot.new()
-    text(0.5, 0.5, "Environmental space", cex = 1.3)
-    plot.new()
-    text(0.5, 0.5, "Geographic space", cex = 1.3)
-
-    par(mar = c(3.5, 3.5, 0.5, 1))
-  } else {
-    if (all.equal(par()$mfrow, c(1, 1)) == TRUE) {
-      par(mfrow = c(1, 2))
-    }
-    par(mar = c(3.5, 3.5, 0.5, 1), cex = 0.7)
-  }
-
-  ## environmental space
+  ## plot
   plot(master$data_matrix[, evars], col = col_all, pch = pch_all, cex = cex_all,
-       bty = "l", xlab = "", ylab = "")
+       bty = "l", xlab = "", ylab = "", main = main)
   title(xlab = variable_1, line = 2.3, cex.lab = 1.1)
   title(ylab = variable_2, line = 2.3, cex.lab = 1.1)
 
@@ -204,15 +246,85 @@ plot_blocks_EG <- function(master, variable_1, variable_2, region_border = TRUE,
       text(cen[1], cen[2], labels = x, cex = cex_ID, col = col_ID)
     })
   }
+}
 
-  ## geographic space
-  ### box to plot
+
+
+#' @rdname plot_blocks_EG
+#' @export
+#' @usage
+#' plot_blocks_G(master, region_border = TRUE, mask_border = FALSE,
+#'               which = "all", block_ID = FALSE, col_all = NULL,
+#'               col_selected = NULL, col_ID = NULL, cex_all = 0.7,
+#'               cex_selected = 1, cex_ID = 1, pch_all = 16, pch_selected = 16)
+
+plot_blocks_G <- function(master, region_border = TRUE, mask_border = FALSE,
+                          which = "all", block_ID = FALSE, col_all = NULL,
+                          col_selected = NULL, col_ID = NULL, cex_all = 0.7,
+                          cex_selected = 1, cex_ID = 1, pch_all = 16,
+                          pch_selected = 16) {
+  # initial tests
+  if (missing(master)) {
+    stop("Argument 'master' is required to produce the plot.")
+  }
+  if (!class(master)[1] %in% c("master_matrix", "master_selection")) {
+    stop("Object defined in 'master' is not valid, see function's help.")
+  }
+  if (!which %in% c("all", "selected")) {
+    stop("Argument 'which' is not valid, options are 'all' or 'selected'.")
+  } else {
+    if (is.null(master$data_matrix$Block)) {
+      stop("Blocks are not defined in data_matrix of 'master', see function 'make_blocks'.")
+    }
+    if (which == "selected") {
+      if (is.null(master$data_matrix$Selected_blocks)) {
+        stop("Object in 'master' does not contain selected blocks, see function 'block_sample'.")
+      }
+    }
+  }
+
+  # preparing data
+  ublocks <- unique(master$data_matrix$Block)
+  nblocks <- length(ublocks)
+
+  gvars <- c("Longitude", "Latitude")
+
+  # colors
+  col_pal <- colorRampPalette(rev(c("#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb",
+                                    "#41b6c4", "#1d91c0", "#225ea8", "#253494",
+                                    "#081d58")))
+
+  if (is.null(col_all) & is.null(col_selected) & is.null(col_ID)) {
+    if (which == "all") {
+      col_all <- sample(col_pal(nblocks),
+                        nblocks)[as.factor(master$data_matrix$Block)]
+    } else {
+      col_all <- "#E1E1E1"
+      col_selected <- "#3B22CB"
+    }
+    col_ID <- "#000000"
+  } else {
+    if (is.null(col_all)) {
+      if (which == "all") {
+        col_all <- sample(col_pal(nblocks), nblocks)
+      } else {
+        col_all <- "#E1E1E1"
+      }
+    }
+    if (is.null(col_selected)) {
+      col_selected <- "#3B22CB"
+    }
+    if (is.null(col_ID)) {
+      col_ID <- "#000000"
+    }
+  }
+
+  ## box to plot
   boxpam <- t(master$region@bbox)
   boxpam <- sp::SpatialPointsDataFrame(boxpam, data.frame(boxpam),
                                        proj4string = master$region@proj4string)
 
-  ### plot
-  par(mar = rep(0.5, 4))
+  ## plot
   sp::plot(boxpam, col = NA)
   maps::map(fill = TRUE, col = "gray97", lforce = "n",
             border = "gray80", add = TRUE)
@@ -229,8 +341,9 @@ plot_blocks_EG <- function(master, variable_1, variable_2, region_border = TRUE,
 
   ## selected blocks
   if (which == "selected") {
+    sel <- which(master$data_matrix$Selected_blocks == 1)
+    selected_data <- master$data_matrix[sel, ]
     points(selected_data[sel, gvars], pch = pch_selected, cex = cex_selected,
            col = col_selected)
   }
 }
-
